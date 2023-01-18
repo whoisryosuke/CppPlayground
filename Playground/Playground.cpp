3f83e47a40ccf91ae9b56e181450f65f8d777139
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-01-17 22:44:21 -0800

    :sparkles: range based for loops

--- Playground/Playground.cpp
+++ Playground/Playground.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "RangeBasedLoops.cpp"
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+    auto rangeBasedLoopExample = RangeBasedLoops{};
+    rangeBasedLoopExample.Example();
 }
---
+++ Playground/RangeBasedLoops.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+
+
+/// Example of Range Based Loops (aka for loops using "ranges" like vectors)
+class RangeBasedLoops {
+private:
+	std::vector<int> getRates() { return std::vector<int> {1, 1, 2, 3, 5, 8, 13}; }
+
+public:
+
+	void Example() {
+		auto rates = getRates();
+		for (int rate : rates) {
+			std::cout << rate << '\n';
+		}
+		for (int& rate : rates)
+		{
+			rate *= 2;
+		}
+	};
+};
